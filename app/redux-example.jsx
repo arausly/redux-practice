7ffaca746364f1fbe6d5ab88a9653c8fda2562a6
Arausi Daniel <arausid@yahoo.com>
2017-05-18 10:57:28 +0100

    Neatest code ever added action generator

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,14 +1,142 @@
+
 var redux = require('redux');
 console.log('Starting Redux...');
 
-const reducer = (state = {name:"Anonymous"},action) =>{
+
+
+
+// ------------ reducer for movie -------------- //
+//----------------------------------------------//
+let movieId = 1;
+const movieReducer = (state = [],action) =>{
+    switch(action.type){
+        case "ADD_MOVIE": 
+            return [
+                  ...state,
+                   {
+                       id:movieId++,
+                       title:action.title,
+                       genre:action.Genre,
+                   }
+            ]
+        break;
+        case "REMOVE_MOVIE":
+          return [
+                   state.filter((hobby)=> hobby.id !== action.id)
+          ]
+        break;
+        default:
+         return state;
+    }
     
-    return state;
 }
 
+// ------------ reducer for hobbiesReducer -------------- //
+//-------------------------------------------------------//
+ let idHobby = 1;
+ let hobbiesReducer = (state =[],action) =>{
+    
+      switch(action.type){
+         case "ADD_HOBBY" :
+            return [
+                ...state,
+                 {
+                     
+                     id:idHobby++,
+                     hobby:action.hobby
+                 }
+            ]
+         break;
+         default:
+           return state;
+         break; 
+    }
+ }
+ 
+ // ------------ reducer  for name -------------- //
+//-------------------------------------------------------//
 
-var  store = redux.createStore(reducer);
+ let nameReducer = (state = "Anonymous",action) =>{
+    
+      switch(action.type){
+         case "CHANGE_NAME" :
+            return action.name;
+         break;
+         default:
+           return state;
+         break; 
+    }
+ }
 
-var currentState = store.getState();
+let reducer = redux.combineReducers({
+    movies : movieReducer,
+    hobbies: hobbiesReducer,
+    name:    nameReducer
+});
 
-console.log("current state, ", currentState);
+
+
+// action generators addMovie
+//--------------------------
+
+let addMovie = (title, Genre) =>{
+    return{
+               type:"ADD_MOVIE",
+               title,
+               Genre,
+    }
+}
+
+// action generators changeMovie
+//--------------------------
+
+let changeName = (name) =>{
+    return{
+        type:"CHANGE_NAME",
+        name
+    }
+}
+
+// action generators addHobby
+//--------------------------
+
+let addHobby = (hobby) =>{
+    return{
+          type:"ADD_HOBBY",
+          hobby
+    }
+}
+
+// action generators  removeMovie
+//--------------------------
+
+let removeMovie = (id) =>{
+    return{
+        type:"REMOVE_MOVIE",
+        id
+    }
+}
+
+var  store = redux.createStore(reducer,redux.compose(  
+   window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+
+let trashIt = store.subscribe(()=>{
+   let state = store.getState();
+    
+    console.log('New name',state.name);
+    console.log("Hobby",state.hobbies);
+    console.log("movie",state.movies);
+});
+
+//dispatch actions
+//--------------------------
+
+store.dispatch(changeName('Arausi'));
+store.dispatch(changeName('Daniel'));
+store.dispatch(addHobby('coding'));
+store.dispatch(addHobby('Reading'));
+store.dispatch(addMovie("Dead Pool 2","Comic"));
+store.dispatch(addMovie("inception","Serious"));
+store.dispatch(removeMovie(1));
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -2,15 +2,51 @@ var redux = require('redux');
 
 console.log('Starting Redux.....');
 
-const reducer = (state ={searchText:'',showCompleted:false,todos:[]},action) =>{
+let reduxInit = {
+                 searchText:'',
+                 showCompleted:false,
+                 todos:[]
+                };
+
+const reducer = (state = reduxInit, action) =>{
     
-return state;    
+    switch(action.type){
+            case "CHANGE_SEARCH_TEXT":
+              return{
+                 ...state,
+                  searchText:action.searchText
+              }
+            break;
+            default:
+             return state;
+            break;
+    }
 }
 
 
-const store = redux.createStore(reducer);
+const store = redux.createStore(reducer,redux.compose(
+ window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
 let currentState = store.getState();
 
 console.log("Current State, ",currentState);
+
+store.subscribe(()=>{
+    let state = store.getState();
+    document.getElementById('main').innerHTML = state.searchText;
+})
+
+store.dispatch({
+    type:"CHANGE_SEARCH_TEXT",
+    searchText:"Some Text"
+});
+
+store.dispatch({
+    type:"CHANGE_SEARCH_TEXT",
+    searchText:"#tbt"   
+});
+
+
+console.log('search text should have changed to some Text',store.getState());
 
