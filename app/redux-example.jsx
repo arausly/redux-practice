45c66b4235db03adc15385ca68a65db931adf7b2
Arausi Daniel <arausid@yahoo.com>
2017-05-18 15:00:05 +0100

    save

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,79 +1,59 @@
-
 var redux = require('redux');
+var axios = require('axios');
+
+import {
+            fetchingHasCompleted,
+            fetchingHasStarted,
+            addHobby,
+            removeHobby,
+            changeName,
+            removeMovie,
+            addMovie
+       } from './actions/index.jsx';
+var store = require('./store/configureStore.jsx').configure();
+
 console.log('Starting Redux...');
 
 
 
-
-let reducer = redux.combineReducers({
-    movies : movieReducer,
-    hobbies: hobbiesReducer,
-    name:    nameReducer
-});
-
-
-
-// action generators addMovie
-//--------------------------
-
-let addMovie = (title, Genre) =>{
-    return{
-               type:"ADD_MOVIE",
-               title,
-               Genre,
-    }
+let fetching = () =>{
+    store.dispatch(fetchingHasStarted());
+    
+//    fetch("http://ipinfo.io/developers",{method:"GET"}).then(res => res.json()).then(json =>{
+//        let jsonObj = JSON.parse(json);
+//        let loc = jsonObj.data.loc;
+//        let base_url ="https://maps.google.com?q=";
+//        
+//         store.dispatch(fetchingHasCompleted(base_url + loc));
+//    });
+    axios.get("http://ipinfo.io").then(json =>{
+        let loc = json.data.loc;
+        let base_url ="https://maps.google.com?q=";
+        
+        store.dispatch(fetchingHasCompleted(base_url+loc));
+    });
 }
 
-// action generators changeMovie
-//--------------------------
-
-let changeName = (name) =>{
-    return{
-        type:"CHANGE_NAME",
-        name
-    }
-}
-
-// action generators addHobby
-//--------------------------
-
-let addHobby = (hobby) =>{
-    return{
-          type:"ADD_HOBBY",
-          hobby
-    }
-}
-
-// action generators  removeMovie
-//--------------------------
-
-let removeMovie = (id) =>{
-    return{
-        type:"REMOVE_MOVIE",
-        id
-    }
-}
-
-var  store = redux.createStore(reducer,redux.compose(  
-   window.devToolsExtension ? window.devToolsExtension() : f => f
-));
-
-
+fetching();
 let trashIt = store.subscribe(()=>{
    let state = store.getState();
     
-    console.log('New name',state.name);
-    console.log("Hobby",state.hobbies);
-    console.log("movie",state.movies);
+    if(state.map.isFetching){
+        document.getElementById('main').innerHTML ="Loading...";
+    }else{
+        document.getElementById("main").innerHTML ="<a href='" + state.map.url + "' target ='_blank'>View your location</a>"
+    }
 });
 
 //dispatch actions
 //--------------------------
 
+
 store.dispatch(changeName('Arausi'));
 store.dispatch(changeName('Daniel'));
 store.dispatch(addHobby('coding'));
 store.dispatch(addHobby('Reading'));
+store.dispatch(removeHobby(2));
 store.dispatch(addMovie("Dead Pool 2","Comic"));
 store.dispatch(addMovie("inception","Serious"));
 store.dispatch(removeMovie(1));
